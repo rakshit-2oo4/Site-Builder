49a6c422bafeeac139555d7ff208bad00d40a6be
Rakshit Sharma <rakshits.123456@gmail.com>
2026-02-14 13:02:02 +0530

    Update redirectTo and callbackURL in AuthPage

--- client/src/pages/auth/AuthPage.tsx
+++ client/src/pages/auth/AuthPage.tsx
@@ -7,7 +7,8 @@ export default function AuthPage() {
   return (
     <main className="p-6 flex flex-col items-center justify-center h-[80vh]">
       <AuthView pathname={pathname}
-        redirectTo="/projects"
+        redirectTo={window.location.origin + "/projects"}
+        callbackURL={window.location.origin + "/projects"}
         classNames={{base: 'bg-black/10 ring ring-indigo-900'}}/>
     </main>
   )
