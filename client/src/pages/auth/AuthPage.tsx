13b922814e757d45d8ef28148df70bfd316b2eeb
Rakshit Sharma <rakshits.123456@gmail.com>
2026-02-14 13:11:58 +0530

    Refactor AuthPage to simplify redirect URLs

--- client/src/pages/auth/AuthPage.tsx
+++ client/src/pages/auth/AuthPage.tsx
@@ -7,8 +7,7 @@ export default function AuthPage() {
   return (
     <main className="p-6 flex flex-col items-center justify-center h-[80vh]">
       <AuthView pathname={pathname}
-        redirectTo={window.location.origin + "/projects"}
-        callbackURL={window.location.origin + "/projects"}
+        redirectTo="/projects"
         classNames={{base: 'bg-black/10 ring ring-indigo-900'}}/>
     </main>
   )
